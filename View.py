599147e4b1ce93969a5c6bc4b64eea39d7201609
Tomasz Ferenc <tomasz.ferenc@technipfmc.com>
2024-10-30 15:10:40 +0100

    fix issue with responsivity in the view page

--- View.py
+++ View.py
@@ -21,35 +21,35 @@ class View():
                 end_date = st.date_input("End Date", current_sprint_end)
 
         last_activity = taskm.last_activity(start_date, end_date)
-        with st.container(border=True):
-            con_col1, con_col2 = st.columns(2)
-            con_col1.write("Last Activity")
-            
+        with st.container(border=True):          
             if datetime.date.today() == last_activity:
-                con_col2.markdown(f'**:green[{last_activity}]** ')
+                st.markdown(f'Last Activity &emsp;&emsp; **:green[{last_activity}]** ')
             else:
-                con_col2.markdown(f'**:red[{last_activity}]** ')
+                st.markdown(f'Last Activity &emsp;&emsp; **:red[{last_activity}]** ')
         with st.container(border=True):
             st.markdown(f"<h5 style='text-align: center;'>Current Sprint: from {start_date} to {end_date}</h5>", unsafe_allow_html=True)    
-            con_col1, con_col2 = st.columns(2)
-            con_col1.write("Stories")
 
             story_all = taskm.story_count(start_date, end_date)
             story_completed = taskm.story_count(start_date, end_date,is_completed=True)
             task_all = taskm.task_count(start_date, end_date,is_completed=False)
             task_completed = taskm.task_count(start_date, end_date,is_completed=True)
-             
-            con_col1.text(f"{story_completed}/{story_all}")
-            con_col2.write("Tasks")    
-            con_col2.text(f"{task_completed}/{task_all}")   
+            coll_1 = st.columns(2)
+            coll_2 = st.columns(2)
+            coll_1[0].markdown(f"Stories:")
+            coll_1[1].markdown(f"{task_completed}/{task_all}")    
+            coll_2[0].markdown(f"Tasks")    
+            coll_2[1].markdown(f"{story_completed}/{story_all}")
+
         with st.container(border=True):
             st.markdown(f"<h5 style='text-align: center;'>Current Sprint: from {start_date} to {end_date}</h5>", unsafe_allow_html=True)       
-            con_col1, con_col2 = st.columns(2)
+  
 
             story_list = taskm.stories_squeeze(start_date, end_date)
             for story_name in story_list:
-                con_col1.text(story_name.name)
-                con_col2.text(f'{story_name.task_count(True)}/{story_name.task_count()}')    
+                col = st.columns(2)
+                col[0].markdown(f'{story_name.name}')
+                col[1].markdown(f'{story_name.task_count(True)}/{story_name.task_count()}')
+                
   
     
 taskm = tm.TaskManagement()
