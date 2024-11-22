886ca5acae13efd9491e5cfee8fc77bb12b9394e
Tomasz Ferenc <tomasz.ferenc@technipfmc.com>
2024-11-22 14:30:53 +0100

    finish front end for an edit page

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-a# task_management
+# task_management
+
+## baklog
+### [tm-1] 
+    Fulfill all information if story is selected.
+    Clean all information if the "add new story" is selected
+### [tm-2] 
+    Make button Add New story working
+### [tm-3] 
+    Add functionality when story is selected list of task is changing
+### [tm-4] 
+    Fulfill all information if story and task is selected.
+    Clean all information if the "add new task" is selected
+### [tm-5] 
+    Make button Save / Add working
--- pages/Edit.py
+++ pages/Edit.py
@@ -18,43 +18,17 @@ class Edit():
         with epic_col[1].expander("Edit Epic"):
             new_epic_name = self.edit_epic(selected_epic)
         with epic_col[1].expander("Add New Story"):
-           st.write("cos")
-        st.text("--Story List--")
-        story_col = st.columns([0.02, 0.98])   
-        with story_col[1].expander("Story 1"):
-            st.button("Edit Story")
-            st.button("Add New Task")
-            st.markdown("---")
-            st.button("Task 1 edit/remove")
-            st.button("Task 2 edit/remove")
-            st.button("Task 3 edit/remove")
+            self.add_new_story(selected_epic)
+
+        story_col = st.columns([0.02, 0.98]) 
+
+        with story_col[1].expander("Add/Edit Task"):
+            self.add_edit_task(selected_epic)
             
 
-        with st.sidebar:
-
-            st.markdown("---")
-            st.write("Edit Story")
-            st.text_input("Name", key="story_name")
-            st.text_input("Description", key="story_description")
-            st.date_input("Est Start Date")
-            st.date_input("Est End Date")
-
-            st.markdown("---")
-            st.write("Task 1 Edit")
-            st.text_input("Task Name",disabled=True, value="Task 1")
-            st.text_input("Description")
-            is_completed = st.checkbox("Is Completed")
-            if is_completed:
-                ic_col0, ic_col1 = st.columns([0.2,1.8])
-                with ic_col1:
-                    st.date_input("Complitation Date")
-            st.button("Save")
-            st.markdown("---")
-            st.button("Remove")
+       
 
     def edit_epic(self, selected_epic):       
-
-        
         new_epic_name = st.text_input("Name",value=selected_epic)
         if st.button("Save Epic"):
 
@@ -65,5 +39,45 @@ class Edit():
 
                         ep.name = new_epic_name
                         self.tasktm.save()
+                        st.rerun()
 
+
+    def add_new_story(self, selected_epic):
+        st.selectbox("Pick the story",['Add new story','Story 1', 'Story 2', 'Story 3'])       
+        story_name = st.text_input("Name",)
+        story_description = st.text_input("Description", )
+        story_est_start_date = st.date_input("Est Start Date")
+        story_est_end_date = st.date_input("Est End Date")
+        if st.button("Add New Story"):
+            validation_text = self.story_validation(story_name, story_est_start_date, story_est_end_date)
+            if validation_text=='Success':
+                st.success("Story was added")
+                time.sleep(3)
+                st.rerun()
+            else:
+                
+                st.error(f"Adding new story has been failed - {validation_text}")
+
+    def story_validation(self, story_name, story_est_start_date, story_est_end_date):
+        return 'Success'
+    
+
+    def add_edit_task(self, selected_epic):       
+        st.selectbox("Pick the story", ['Story 1', 'Story 2', 'Story 3'])
+        st.selectbox('Pick the task',['Add new task', 'task 2', 'task 3',]) 
+        task_name = st.text_input("Task Name")
+        task_description  = st.text_input("Desclription ")
+        task_is_completed = st.checkbox("Is completed")
+        task_complitation_date = st.date_input("Complitation date")
+        task_is_cancelled= st.checkbox("Is cancelled")
+        task_validation = self.task_validation()
+        if st.button("Save / Add"):
+            if task_validation=='Success':
+                st.success("Story was added")
+            else:
+                st.error(f"Adding new story has been failed - {task_validation}")
+
+
+    def task_validation(self):
+        return 'Success'
 start = Edit(tasktm)
