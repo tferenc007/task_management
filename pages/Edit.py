ced608a1c3aaaebebbc63544c2129b2bf79b238c
Tomasz Ferenc <tomasz.ferenc@technipfmc.com>
2025-03-11 16:03:33 +0100

    fix issue with sprint ids in edit section

--- pages/Edit.py
+++ pages/Edit.py
@@ -81,8 +81,10 @@ class Edit():
             epic_name = st.selectbox("Epic Name",epic_list_origin, key='edit_story_epic_name', index=epic.epic_index)
             story_name = st.text_input("Name",value=picked_story_obj.name)
             story_description = st.text_input("Description",value=picked_story_obj.description)
-            sprint_id = st.selectbox("Sprint", tasktm.dic_sprint.keys(), index=picked_story_obj.sprint_index)
-           
+            sprint_id = st.selectbox("Sprint", tasktm.dic_sprint.keys(), index=list(tasktm.dic_sprint.keys()).index(picked_story_obj.sprint_id))
+
+   
+
             sprint_start_date = datetime.strptime(tasktm.dic_sprint[sprint_id]['sprint_start_date'], "%Y-%m-%d")
             sprint_end_date = datetime.strptime(tasktm.dic_sprint[sprint_id]['sprint_end_date'], "%Y-%m-%d")
             story_points = st.selectbox("Story Points", ["1",'3','5','8','13','21'], index=picked_story_obj.story_point_index)
